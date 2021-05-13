ef15c8a626c5060bee9b61ba61a3c1866c8192f3
MelvynHerzig <melvyn.herzig@heig-vd.ch>
2021-05-13 21:43:23 +0200

    Préparation des sprites et des tuiles

---
+++ App/core/assets/Map/tiles.tsx
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="tiles" tilewidth="64" tileheight="64" tilecount="7" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <properties>
+   <property name="type" value="wall"/>
+  </properties>
+  <image width="64" height="64" source="border.png"/>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="type" value="collectible"/>
+  </properties>
+  <image width="64" height="64" source="collectible.png"/>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="type" value="fire"/>
+  </properties>
+  <image width="64" height="64" source="fire.png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="64" source="pathFire.png"/>
+ </tile>
+ <tile id="4">
+  <image width="64" height="64" source="path.png"/>
+ </tile>
+ <tile id="5">
+  <image width="64" height="64" source="grass.png"/>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="type" value="movable"/>
+  </properties>
+  <image width="64" height="64" source="movable.png"/>
+ </tile>
+</tileset>
